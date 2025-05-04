4564dcdd3860418154d12f2ebb607f9e5f5361db
Nakul Pampaniya <pampaniyanakul1997@gmail.com>
2025-05-04 12:09:48 +0530

    change message in js file

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('contact-form').addEventListener('submit', function(e) {
   e.preventDefault(); // Prevent real form submission
   
   // Simple alert (later we can connect real backend or email service)
-  alert('Thank you for your message, ' + document.getElementById('name').value + '!');
+  alert('Thank you for your message, ' + document.getElementById('name').value + '!!');
   
   // Clear form
   document.getElementById('contact-form').reset();
